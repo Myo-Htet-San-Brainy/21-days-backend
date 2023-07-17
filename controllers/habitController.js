17d42d1b9df56683e2462dae4d70732c9b2e27c4
BrainyJJ <myohtetsandrinksmilk@gmail.com>
2023-07-17 08:12:52 +0700

    Added pagination to getAllHabits api call

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -8,10 +8,15 @@ const customError = require("../errors");
 const checkOwner = require("../utils/checkOwner");
 
 const getAllHabits = async (req, res) => {
-  const habits = await Habit.find({}).select(
-    "-timeToSendReminder -user.userEmail"
-  );
-  res.status(StatusCodes.OK).json({ data: habits, message: "All Habits" });
+  let habit = Habit.find({}).select("-timeToSendReminder -user.userEmail");
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = 10;
+  const skip = (page - 1) * limit;
+  habit = habit.skip(skip).limit(limit);
+
+  const habits = await habit;
+  res.status(StatusCodes.OK).json({ count: habits.length, data: habits });
 };
 
 const createHabit = async (req, res) => {
@@ -32,6 +37,7 @@ const createHabit = async (req, res) => {
 
   //add user
   const user = await User.findById(req.user.userId);
+
   req.body.user = {
     userId: user._id,
     username: user.username,
--- models/habitModel.js
+++ models/habitModel.js
@@ -32,18 +32,23 @@ const habitSchema = new mongoose.Schema({
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: [true, "Please provide the habit creator."],
+      // default: new mongoose.Types.ObjectId("64aa649eda1d5f341385ad89"),
       ref: "User",
     },
     username: {
       type: String,
       required: [true, "Please provide the habit creator name."],
+      // default: "soungoolwin",
     },
     userImage: {
       type: String,
       required: [true, "Please provide the habit creator image."],
+      // default:
+      //   "https://res.cloudinary.com/dhwwdk7uq/image/upload/v1689521881/21daysUserImages/tmp-1-1689521878862_bvnlaf.jpg",
     },
     userEmail: {
       type: String,
+      // default: "soungoolwin275@gmail.com",
     },
   },
   timeToSendReminder: {
